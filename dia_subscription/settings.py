f7396e63976df2899ce430dc213843f787b7e467
hordii.rushynets@internet-techonlogy-hub.com <hordii.rushynets@internet-techonlogy-hub.com>
2024-10-10 16:13:11 +0300

    fix

--- dia_subscription/settings.py
+++ dia_subscription/settings.py
@@ -88,6 +88,8 @@ DATABASES = {
     }
 }
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
