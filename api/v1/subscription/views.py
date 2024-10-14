4708879f2d9438a02c61f8c1327be6e05c4a139b
hordii.rushynets@internet-techonlogy-hub.com <hordii.rushynets@internet-techonlogy-hub.com>
2024-10-14 21:14:07 +0300

    Add print of decode signature

--- api/v1/subscription/views.py
+++ api/v1/subscription/views.py
@@ -8,12 +8,14 @@ import json
 
 class SuccessView(APIView):
     def post(self, request: Request, *args, **kwargs) -> Response:
-        print("Encoded request data:", request.data , "\n\n")
         encoded_data = request.data.get('encodeData')
-
         decoded_data = base64.b64decode(encoded_data).decode('utf-8')
-        print("Decoded request data:", decoded_data, "\n\n")
 
         data = json.loads(decoded_data)
         print("Data:", data, "\n\n")
+
+        signature = data.get('signature')
+        decoded_signature = base64.b64decode(signature).decode('utf-8')
+        print("Decoded signature:", json.loads(decoded_signature), "\n")
+
         return Response({"success": True}, status=status.HTTP_200_OK)
