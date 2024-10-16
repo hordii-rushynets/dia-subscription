b59a861593eed1d5391a101265d0e65295b5f1a2
hordii.rushynets@internet-techonlogy-hub.com <hordii.rushynets@internet-techonlogy-hub.com>
2024-10-16 13:32:31 +0300

    UPDATE show Signer Count in admin

--- apps/dia_subscription_users/admin.py
+++ apps/dia_subscription_users/admin.py
@@ -1,6 +1,15 @@
-from django.contrib import admin 
+from django.contrib import admin
+
 from apps.dia_subscription_users import models
 
-admin.site.register(models.Company)
-admin.site.register(models.Signer)
 
+class CompanyAdmin(admin.ModelAdmin):
+    # Define a method to count related signers
+    def signer_count(self, obj):
+        return models.Signer.objects.filter(vote_business=obj).count() + models.Signer.objects.filter(vote_business_veterans=obj).count()
+
+    signer_count.short_description = 'Signer Count'
+    list_display = ('name', 'business_type', 'signer_count')
+
+admin.site.register(models.Company, CompanyAdmin)
+admin.site.register(models.Signer)
